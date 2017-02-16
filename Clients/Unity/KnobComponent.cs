c2efb0c10b2dc68854c6f8dab2ffc71064beb5cd
Chris Wade <cjacobwade@gmail.com>
2017-02-16 17:07:01 -0600

    Updates naming to match Unity/C# standard

--- Clients/Unity/KnobComponent.cs
+++ Clients/Unity/KnobComponent.cs
@@ -1,129 +1,123 @@
-﻿using System;
+using System;
 using System.Net.Sockets;
 using System.Threading;
 using UnityEngine;
 using System.Linq;
 
-public class KnobComponent : MonoBehaviour {
+public class KnobComponent : MonoBehaviour
+{
+	const int _numKnobs = 24;
+	float[] _knobs = new float[_numKnobs];
 
-    const int numKnobs = 24;
-    float[] knobs = new float[numKnobs];
+	void Start()
+	{
+		Knobs.Start(_numKnobs);
+	}
 
-    void Start()
-    {
-        Knobs.start(numKnobs);
-    }
+	void Update()
+	{
+		for (int i = 0; i < _numKnobs; i++)
+		{
+			_knobs[i] = Knobs.Get(i);
+			Shader.SetGlobalFloat("Knob" + i.ToString(), _knobs[i]);
+		}
+	}
 
-    void Update()
-    {
-        for (int i = 0; i < numKnobs; i++)
-        {
-            knobs[i] = Knobs.get(i);
-            Shader.SetGlobalFloat("Knob" + i.ToString(), knobs[i]);
-        }
-    }
-
-    void OnDestroy()
-    {
-        print(String.Join("\n", Enumerable.Range(0, numKnobs).Select(i => String.Format("{0}:\t{1}", i, Knobs.get(i))).ToArray()));
-        Knobs.stop();
-    }
+	void OnDestroy()
+	{
+		print(String.Join("\n", Enumerable.Range(0, _numKnobs).Select(i => String.Format("{0}:\t{1}", i, Knobs.Get(i))).ToArray()));
+		Knobs.Stop();
+	}
 }
-
-
-
-
 
 public class Knobs
 {
-    private static int size;
-    public static int[] knobs;
+	static int _size;
+	public static int[] knobs;
 
-    private static Socket sock;
+	static Socket _sock;
 
-    private static Thread worker;
-    private static bool running = false;
+	static Thread _worker;
+	static bool _running = false;
 
-    public static void start(int numKnobs)
-    {
-        if (!running)
-        {
-            size = numKnobs;
-            knobs = new int[size];
+	public static void Start(int numKnobs)
+	{
+		if (!_running)
+		{
+			_size = numKnobs;
+			knobs = new int[_size];
 
-            try
-            {
-                sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                sock.Connect("localhost", 8008);
+			try
+			{
+				_sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+				_sock.Connect("localhost", 8008);
 
-                worker = new Thread(new ThreadStart(readFromServer));
-                worker.Start();
+				_worker = new Thread(new ThreadStart(ReadFromServer));
+				_worker.Start();
 
-                running = true;
-            }
-            catch (SocketException e)
-            {
-                Console.WriteLine(e);
-            }
-        }
-    }
+				_running = true;
+			}
+			catch (SocketException e)
+			{
+				Console.WriteLine(e);
+			}
+		}
+	}
 
-    public static void stop()
-    {
-        if (running)
-        {
-            sock.Shutdown(SocketShutdown.Both);
-            sock.Close();
-            running = false;
-        }
-    }
+	public static void Stop()
+	{
+		if (_running)
+		{
+			_sock.Shutdown(SocketShutdown.Both);
+			_sock.Close();
+			_running = false;
+		}
+	}
 
-    public static float get(int index)
-    {
-        return get(index, 0, 1);
-    }
+	public static float Get(int index)
+	{
+		return Get(index, 0, 1);
+	}
 
-    public static float get(int index, float max)
-    {
-        return get(index, 0, max);
-    }
+	public static float Get(int index, float max)
+	{
+		return Get(index, 0, max);
+	}
 
-    public static float get(int index, float min, float max)
-    {
-        float value = (float)knobs[index] / 127.0f;
-        return (1 - value) * min + value * max;
-    }
+	public static float Get(int index, float min, float max)
+	{
+		float value = (float)knobs[index] / 127.0f;
+		return (1 - value) * min + value * max;
+	}
 
 
-    static void readFromServer()
-    {
-        byte[] sockBuffer = new byte[size];
+	static void ReadFromServer()
+	{
+		byte[] sockBuffer = new byte[_size];
 
-        try
-        {
-            while (running)
-            {
-                if (sock.Receive(sockBuffer, size, SocketFlags.None) > 0)
-                {
-                    for (int i = 0; i < size; i++)
-                    {
-                        knobs[i] = sockBuffer[i];
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Socket connection lost; aborting");
-                    sock.Shutdown(SocketShutdown.Both);
-                    sock.Close();
-                    break;
-                }
-            }
-        }
-        catch (SocketException e)
-        {
-            Console.WriteLine(e);
-        }
-    }
+		try
+		{
+			while (_running)
+			{
+				if (_sock.Receive(sockBuffer, _size, SocketFlags.None) > 0)
+				{
+					for (int i = 0; i < _size; i++)
+					{
+						knobs[i] = sockBuffer[i];
+					}
+				}
+				else
+				{
+					Console.WriteLine("Socket connection lost; aborting");
+					_sock.Shutdown(SocketShutdown.Both);
+					_sock.Close();
+					break;
+				}
+			}
+		}
+		catch (SocketException e)
+		{
+			Console.WriteLine(e);
+		}
+	}
 }
-
-
